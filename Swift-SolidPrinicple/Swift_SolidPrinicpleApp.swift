e219125d8cc1cd1c64c4514bad596b1e0eb020c7
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-02-07 13:19:55 -0800

    Initial Commit

---
+++ Swift-SolidPrinicple/Swift_SolidPrinicpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SolidPrinicpleApp.swift
+//  Swift-SolidPrinicple
+//
+//  Created by ReetDhillon on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct Swift_SolidPrinicpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
